24893b94fac676bef016a1627188fb5a9a5a8d69
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2024-08-19 17:14:32 +0100

    Updated

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import asyncio
+import time
+
+# Import the wait_n function from the module '1-concurrent_coroutines'
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measure the average time taken by the wait_n function to execute.
+
+    Args:
+        n (int): The number of concurrent tasks to run.
+        max_delay (int): The maximum delay value for each task.
+
+    Returns:
+        float: The average time taken per task.
+    """
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+
+    # Calculate total elapsed time and average time per task
+    total_time = end_time - start_time
+    return total_time / n
---
+++ 0x01-python_async_function/new.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import asyncio
+from typing import List
+
+myName: str = 'Yusuf'
+arr:List[str] = []
+arr = [myName for _ in range(5)]
+print(arr)
